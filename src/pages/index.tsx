70a327dbdd9fac3c9236e4703e5a496b6f64e059
Nivlake <sadiratarunacalvindoro@gmail.com>
2023-02-08 14:49:39 +0700

    delete sidebar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-import Sidebar from 'components/Sidebar'
-import Sidebar_2 from 'components/Sidebar_2'
-import Sidebar_3 from 'components/Sidebar_3'
 
 const inter = Inter({ subsets: ['latin'] })
 
