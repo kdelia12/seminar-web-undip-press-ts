ac1a638237017d482a3145d283c4ef326c679529
faturrahmanalharitszoe <faturrahmanalharitszoe@gmail.com>
2023-02-16 11:38:45 +0700

    Updating Admin Pages

--- src/pages/Admin/seminar.tsx
+++ src/pages/Admin/seminar.tsx
@@ -10,76 +10,114 @@ export default function Seminar(){
                 </aside>
                 <div className="w-full flex flex-col p-8 gap-6">
                     <h1 className="text-3xl font-semibold px-2.5">Manajemen Seminar</h1>
-                    <div className="container p-2.5">
-                        {/* Card */}
-                        <Link href="/Admin/seminar" className="w-fit flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl bg-neutral-300 hover:bg-neutral-400">
-                            <div className="flex flex-row items-center justify-between py-7 px-[4.188rem] leading-normal">
-                                <img src="/icon/seminar.svg" alt="" />
-                                <h5 className="ml-3 text-2xl font-bold tracking-tight text-black">Buat Seminar Baru</h5>
-                            </div>
-                        </Link>
-                    </div>
-                    <div className="flex flex-col gap-2.5">
-                        <div className="flex flex-row p-2.5">
-                            <h1 className="text-2xl font-bold">List Seminar</h1>
-                            <form className="ml-auto">
-                                <div className="flex">
-                                    <div className="relative w-80">
-                                        <input className="block p-2.5 w-full z-20 text-sm text-neutral-500 rounded-lg border-neutral-500 border-2 focus:ring-blue-500 focus:border-blue-500" placeholder="Search"/>
-                                        <button type="submit" className="absolute top-0 right-0 mr-1 p-2.5 text-sm font-medium text-white">
-                                            <img src="/icon/search.svg" alt="" />
-                                        </button>
+                    <div className="container flex flex-row p-2.5 gap-5">
+                        <div className="w-[35.125rem] flex flex-col gap-[1.875rem]">
+                            {/* Card */}
+                            <Link href="/Admin/Seminar/Create" className="w-full flex justify-center bg-primary-300 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-primary-400">
+                                <div className="flex flex-row items-center py-7 px-[4.188rem] leading-normal">
+                                    <img src="/icon/seminar.svg" alt="" />
+                                    <h5 className="ml-3 text-2xl font-bold tracking-tight text-black">Buat Seminar Baru</h5>
+                                </div>
+                            </Link>
+                            <div className="w-full flex flex-col p-5 gap-2.5 bg-primary-300 rounded-lg justify-between">
+                                <div className="w-full flex justify-between">
+                                    <h2 className="font-bold text-xl">Upcoming Seminar</h2>
+                                    <button><h3 className="font-bold text-medium underline underline-offset-1">see all</h3></button>
+                                </div>
+                                {/* Bagian Bawah */}
+                                <div className="flex flex-col border-t-2 border-black">
+                                    <div className="flex justify-between">
+                                        <div className="flex flex-col">
+                                            <h2 className="font-bold text-base">Judul Seminar</h2>
+                                            <h3 className="font-medium text-sm">Pembicara</h3>
+                                        </div>
+                                        <div>
+                                            <h2 className="font-bold text-base">Tanggal Dilaksanakan</h2>
+                                            <h3 className="font-medium text-sm text-right">03 Februari 2023</h3>
+                                        </div>
                                     </div>
                                 </div>
-                            </form>
+                                <div className="flex flex-col border-t-2 border-black">
+                                    <div className="flex justify-between">
+                                        <div className="flex flex-col">
+                                            <h2 className="font-bold text-base">Judul Seminar</h2>
+                                            <h3 className="font-medium text-sm">Pembicara</h3>
+                                        </div>
+                                        <div>
+                                            <h2 className="font-bold text-base">Tanggal Dilaksanakan</h2>
+                                            <h3 className="font-medium text-sm text-right">03 Februari 2023</h3>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="flex flex-col border-t-2 border-black">
+                                    <div className="flex justify-between">
+                                        <div className="flex flex-col">
+                                            <h2 className="font-bold text-base">Judul Seminar</h2>
+                                            <h3 className="font-medium text-sm">Pembicara</h3>
+                                        </div>
+                                        <div>
+                                            <h2 className="font-bold text-base">Tanggal Dilaksanakan</h2>
+                                            <h3 className="font-medium text-sm text-right">03 Februari 2023</h3>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="flex flex-col border-t-2 border-black">
+                                    <div className="flex justify-between">
+                                        <div className="flex flex-col">
+                                            <h2 className="font-bold text-base">Judul Seminar</h2>
+                                            <h3 className="font-medium text-sm">Pembicara</h3>
+                                        </div>
+                                        <div>
+                                            <h2 className="font-bold text-base">Tanggal Dilaksanakan</h2>
+                                            <h3 className="font-medium text-sm text-right">03 Februari 2023</h3>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="flex flex-col border-t-2 border-black">
+                                    <div className="flex justify-between">
+                                        <div className="flex flex-col">
+                                            <h2 className="font-bold text-base">Judul Seminar</h2>
+                                            <h3 className="font-medium text-sm">Pembicara</h3>
+                                        </div>
+                                        <div>
+                                            <h2 className="font-bold text-base">Tanggal Dilaksanakan</h2>
+                                            <h3 className="font-medium text-sm text-right">03 Februari 2023</h3>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                            {/* Card */}
-                            <div className="bg-neutral-300 rounded-lg px-2.5">
-                                <div className="flex flex-row items-center p-2.5 leading-normal">
-                                    <div className="gap-2.5 flex flex-row">
-                                        <img src="/index1.svg" alt="" />
-                                        <div className="flex flex-col p-2.5 gap-2.5">
-                                            <div className="flex flex-row gap-2.5">
-                                                <h3 className="w-fit px-2.5 rounded-lg border border-black border-2">Teknologi</h3>
-                                            </div>
-                                            <h1 className="text-5xl font-bold tracking-tight text-black">Judul Seminar</h1>
-                                            <h2 className="">Penyelenggara Seminar</h2>
-                                        </div> 
+                        {/* Kanan */}
+                        <div className="flex flex-col">
+                            <div className="w-[27.875rem] bg-primary-300 rounded-lg p-5">
+                                <div className="w-full flex justify-between mb-2.5">
+                                    <h2 className="font-bold text-xl">Tiket</h2>
+                                    <button><h3 className="font-bold text-medium underline underline-offset-1">see all</h3></button>
+                                </div>
+                                <div className="flex flex-col gap-2.5">
+                                    <div className="flex justify-between">
+                                        <h2 className="font-bold text-base">Judul Seminar</h2>
+                                        <h3 className="font-medium text-sm text-right">57 Tiket Tersisa</h3>
                                     </div>
-                                    <div className="flex flex-col ml-auto gap-2.5 p-2.5 text-right">
-                                        <h2>Tanggal Diselenggarakan</h2>
-                                        <h2>02 Januari 2023</h2>
-                                        <div className="flex flex-row gap-2.5 justify-end">
-                                            <button><img src="/icon/edit.svg" alt="" /></button>
-                                            <button><img src="/icon/delete.svg" alt="" /></button>
-                                        </div>
+                                    <div className="flex justify-between">
+                                        <h2 className="font-bold text-base">Judul Seminar</h2>
+                                        <h3 className="font-medium text-sm text-right">57 Tiket Tersisa</h3>
+                                    </div>
+                                    <div className="flex justify-between">
+                                        <h2 className="font-bold text-base">Judul Seminar</h2>
+                                        <h3 className="font-medium text-sm text-right">57 Tiket Tersisa</h3>
+                                    </div>
+                                    <div className="flex justify-between">
+                                        <h2 className="font-bold text-base">Judul Seminar</h2>
+                                        <h3 className="font-medium text-sm text-right">57 Tiket Tersisa</h3>
+                                    </div>
+                                    <div className="flex justify-between">
+                                        <h2 className="font-bold text-base">Judul Seminar</h2>
+                                        <h3 className="font-medium text-sm text-right">57 Tiket Tersisa</h3>
                                     </div>
                                 </div>
                             </div>
-                            {/* Card */}
-                            <div className="bg-neutral-300 rounded-lg px-2.5">
-                                <div className="flex flex-row items-center p-2.5 leading-normal">
-                                    <div className="gap-2.5 flex flex-row">
-                                        <img src="/index1.svg" alt="" />
-                                        <div className="flex flex-col p-2.5 gap-2.5">
-                                            <div className="flex flex-row gap-2.5">
-                                                <h3 className="w-fit px-2.5 rounded-lg border border-black border-2">Teknologi</h3>
-                                            </div>
-                                            <h1 className="text-5xl font-bold tracking-tight text-black">Judul Seminar</h1>
-                                            <h2 className="">Penyelenggara Seminar</h2>
-                                        </div> 
-                                    </div>
-                                    <div className="flex flex-col ml-auto gap-2.5 p-2.5 text-right">
-                                        <h2>Tanggal Diselenggarakan</h2>
-                                        <h2>02 Januari 2023</h2>
-                                        <div className="flex flex-row gap-2.5 justify-end">
-                                            <button><img src="/icon/edit.svg" alt="" /></button>
-                                            <button><img src="/icon/delete.svg" alt="" /></button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                                
+                        </div>
                     </div> 
                 </div>
             </div>
